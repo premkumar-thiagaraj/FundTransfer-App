fffa3886eeb0cd136326545cc8e925105b37ddd2
Prem kumar <premkumar-thigaraj@github.com>
2021-10-23 02:02:39 -0700

    fix scripts

--- app/src/main/java/com/fundtransfer/hcl/app/model/Account.java
+++ app/src/main/java/com/fundtransfer/hcl/app/model/Account.java
@@ -1,10 +1,18 @@
 package com.fundtransfer.hcl.app.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="ACCOUNT")
 public class Account {
 
+	@Id
+	@Column(name="ACCOUNT_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 }
