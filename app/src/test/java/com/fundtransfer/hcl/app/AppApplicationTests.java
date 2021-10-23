e3510596147b978710cc836dbd87e545b1e870f7
Prem kumar <premkumar-thigaraj@github.com>
2021-10-22 23:34:49 -0700

    initial project commit

---
+++ app/src/test/java/com/fundtransfer/hcl/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundtransfer.hcl.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
